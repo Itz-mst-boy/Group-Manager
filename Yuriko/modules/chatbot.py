912c817734933ab7606a8f4acd21b654f040b3fa
ᴍᴜᴋᴇsʜ <100356452+Itz-mst-boy@users.noreply.github.com>
2022-05-14 22:45:44 +0530

    Add files via upload

---
+++ Yuriko/modules/chatbot.py
@@ -0,0 +1,175 @@
+import json
+import re
+import os
+import html
+import requests
+import Yuriko.modules.sql.chatbot_sql as sql
+
+from time import sleep
+from telegram import ParseMode
+from telegram import (CallbackQuery, Chat, MessageEntity, InlineKeyboardButton,
+                      InlineKeyboardMarkup, Message, Update, Bot, User)
+from telegram.ext import (CallbackContext, CallbackQueryHandler, CommandHandler,
+                          DispatcherHandlerStop, Filters, MessageHandler,
+                          run_async)
+from telegram.error import BadRequest, RetryAfter, Unauthorized
+from telegram.utils.helpers import mention_html, mention_markdown, escape_markdown
+
+from Yuriko.modules.helper_funcs.filters import CustomFilters
+from Yuriko.modules.helper_funcs.chat_status import user_admin, user_admin_no_reply
+from Yuriko import dispatcher, updater, SUPPORT_CHAT
+from Yuriko.modules.log_channel import gloggable
+
+@run_async
+@user_admin_no_reply
+@gloggable
+def kukirm(update: Update, context: CallbackContext) -> str:
+    query: Optional[CallbackQuery] = update.callback_query
+    user: Optional[User] = update.effective_user
+    match = re.match(r"rm_chat\((.+?)\)", query.data)
+    if match:
+        user_id = match.group(1)
+        chat: Optional[Chat] = update.effective_chat
+        is_kuki = sql.rem_kuki(chat.id)
+        if is_kuki:
+            is_kuki = sql.rem_kuki(user_id)
+            return (
+                f"<b>{html.escape(chat.title)}:</b>\n"
+                f"AI_DISABLED\n"
+                f"<b>Admin:</b> {mention_html(user.id, html.escape(user.first_name))}\n"
+            )
+        else:
+            update.effective_message.edit_text(
+                "Mukhushi Robot Chatbot Disabled By {}.".format(mention_html(user.id, user.first_name)),
+                parse_mode=ParseMode.HTML,
+            )
+
+    return ""
+
+@run_async
+@user_admin_no_reply
+@gloggable
+def kukiadd(update: Update, context: CallbackContext) -> str:
+    query: Optional[CallbackQuery] = update.callback_query
+    user: Optional[User] = update.effective_user
+    match = re.match(r"add_chat\((.+?)\)", query.data)
+    if match:
+        user_id = match.group(1)
+        chat: Optional[Chat] = update.effective_chat
+        is_kuki = sql.set_kuki(chat.id)
+        if is_kuki:
+            is_kuki = sql.set_kuki(user_id)
+            return (
+                f"<b>{html.escape(chat.title)}:</b>\n"
+                f"AI_ENABLE\n"
+                f"<b>Admin:</b> {mention_html(user.id, html.escape(user.first_name))}\n"
+            )
+        else:
+            update.effective_message.edit_text(
+                "Mukhushi Robot Chatbot Enabled By {}.".format(mention_html(user.id, user.first_name)),
+                parse_mode=ParseMode.HTML,
+            )
+
+    return ""
+
+@run_async
+@user_admin
+@gloggable
+def kuki(update: Update, context: CallbackContext):
+    user = update.effective_user
+    message = update.effective_message
+    msg = "Choose an option My Boss"
+    keyboard = InlineKeyboardMarkup([[
+        InlineKeyboardButton(
+            text="ᴇɴᴀʙʟᴇ 👻",
+            callback_data="add_chat({})")],
+       [
+        InlineKeyboardButton(
+            text="ᴅɪsᴀʙʟᴇ 💔",
+            callback_data="rm_chat({})")]])
+    message.reply_text(
+        msg,
+        reply_markup=keyboard,
+        parse_mode=ParseMode.HTML,
+    )
+
+def kuki_message(context: CallbackContext, message):
+    reply_message = message.reply_to_message
+    if message.text.lower() == "kuki":
+        return True
+    if reply_message:
+        if reply_message.from_user.id == context.bot.get_me().id:
+            return True
+    else:
+        return False
+        
+
+def chatbot(update: Update, context: CallbackContext):
+    message = update.effective_message
+    chat_id = update.effective_chat.id
+    bot = context.bot
+    is_kuki = sql.is_kuki(chat_id)
+    if not is_kuki:
+        return
+	
+    if message.text and not message.document:
+        if not kuki_message(context, message):
+            return
+        sweetie = message.text
+        bot.send_chat_action(chat_id, action="typing")
+        url = f"https://kukiapi.xyz/api/apikey=1356469075-KUKIkq4WMg5FV4/KIGO/NULL-CODER/message={sweetie}" 
+        request = requests.get(url) 
+        results = json.loads(request.text) 
+        boyresult = f"{results['reply']}"
+        sleep(0.5)
+        message.reply_text(boyresult)
+
+def list_all_chats(update: Update, context: CallbackContext):
+    chats = sql.get_all_kuki_chats()
+    text = "<b>Fallen Enabled Chats</b>\n"
+    for chat in chats:
+        try:
+            x = context.bot.get_chat(int(*chat))
+            name = x.title or x.first_name
+            text += f"• <code>{name}</code>\n"
+        except (BadRequest, Unauthorized):
+            sql.rem_kuki(*chat)
+        except RetryAfter as e:
+            sleep(e.retry_after)
+    update.effective_message.reply_text(text, parse_mode="HTML")
+
+__help__ = """
+✗ `Chatbot utilizes the` *YURIKO* `api which allows Mukhushi to talk and provide a more interactive group chat experience.`
+
+*Admins only Commands*:
+ 
+✗ /Chatbot - `Shows chatbot control panel`
+  
+*✗ Pᴏᴡᴇʀᴇᴅ 💕 Bʏ: @mastermind_network_official!*
+"""
+
+__mod_name__ = "CʜᴀᴛBᴏᴛ"
+
+
+CHATBOTK_HANDLER = CommandHandler("chatbot", kuki )
+ADD_CHAT_HANDLER = CallbackQueryHandler(kukiadd, pattern=r"add_chat" )
+RM_CHAT_HANDLER = CallbackQueryHandler(kukirm, pattern=r"rm_chat" )
+CHATBOT_HANDLER = MessageHandler(
+    Filters.text & (~Filters.regex(r"^#[^\s]+") & ~Filters.regex(r"^!")
+                    & ~Filters.regex(r"^\/")), chatbot, )
+LIST_ALL_CHATS_HANDLER = CommandHandler(
+    "allchats", list_all_chats, filters=CustomFilters.dev_filter, )
+
+dispatcher.add_handler(ADD_CHAT_HANDLER)
+dispatcher.add_handler(CHATBOTK_HANDLER)
+dispatcher.add_handler(RM_CHAT_HANDLER)
+dispatcher.add_handler(LIST_ALL_CHATS_HANDLER)
+dispatcher.add_handler(CHATBOT_HANDLER)
+
+__handlers__ = [
+    ADD_CHAT_HANDLER,
+    CHATBOTK_HANDLER,
+    RM_CHAT_HANDLER,
+    LIST_ALL_CHATS_HANDLER,
+    CHATBOT_HANDLER,
+]
