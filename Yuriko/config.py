7491fb536663865914a452938d86c426829553c7
ᴍᴜᴋᴇsʜ <100356452+Itz-mst-boy@users.noreply.github.com>
2022-05-16 15:30:04 +0530

    Update config.py

--- Yuriko/config.py
+++ Yuriko/config.py
@@ -14,8 +14,8 @@ class Config(object):
     # REQUIRED
     # Login to https://my.telegram.org and fill in these slots with the details given by it
 
-    API_ID = 8884119  # integer value, dont use ""
-    API_HASH = "d94fe90a1d7ee5ce6f36b20d4ca79280"
+    API_ID =   # integer value, dont use ""
+    API_HASH = ""
     TOKEN = "BOT_TOKEN"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
     OWNER_ID = 5142140533  # If you dont know, run the bot and do /id in your private chat with it, also an integer
     OWNER_USERNAME = "@itz_mst_boy"
